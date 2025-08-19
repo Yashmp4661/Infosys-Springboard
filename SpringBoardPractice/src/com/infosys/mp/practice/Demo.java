7a20641402c082968d38c97178934239a91edfcd
Yashmp4661 <yashwanthmp2@gmail.com>
2025-08-19 08:41:47 +0530

    Encapsulation

---
+++ SpringBoardPractice/src/com/infosys/mp/practice/Demo.java
@@ -0,0 +1,19 @@
+package com.infosys.mp.practice;
+
+
+
+public class Demo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob=new Encapsulation();
+		//ob.name="mp";
+	      //ob.age=20;
+		ob.setAge(20);
+		ob.setName("mp");
+		System.out.println(ob);
+		
+
+		
+	}
+
+}
